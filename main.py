c705dc531fdc778a45800c0c1cfe8b1b35981c48
franceballin <michalcolesnikov@yandex.ru>
2023-12-09 17:19:30 +0300

    добавлено окно "Tetris 2077" (файлы main.py и settings.py)

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('тест')
+# Главный файл для запуска игры
+
+import pygame
+from settings import *
+
+# Инициализация Pygame и создание окна
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tetris 2077")
+
+# Основной игровой цикл
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+
+    pygame.display.flip()
+
+pygame.quit()
