0efc07ae5c9b39c115256532f71378389c31e087
franceballin <michalcolesnikov@yandex.ru>
2023-12-09 18:10:40 +0300

    добавил тетрисную сетку

--- board.py
+++ board.py
@@ -1,11 +1,20 @@
 # Модуль для отображения игрового поля
+import pygame
+from settings import *
+
 
 class Board:
     def __init__(self):
-        pass
+        self.grid = [[0 for _ in range(10)] for _ in range(20)]
+        self.block_size = 25
+        self.grid_offset = 50
 
-    def draw_grid(self):
-        pass
+    def draw_grid(self, screen):
+        for y, row in enumerate(self.grid):
+            for x, val in enumerate(row):
+                pygame.draw.rect(screen, (255, 255, 255), ((x * self.block_size) + self.grid_offset,
+                                                           (y * self.block_size) + self.grid_offset,
+                                                           self.block_size, self.block_size), 1)
 
     def update(self):
         pass
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Главный файл для запуска игры
-
 import pygame
 from settings import *
 from game import Game
+from board import Board
 from input_handler import InputHandler
 
 # Инициализация Pygame и создание окна
@@ -12,6 +12,7 @@ pygame.display.set_caption("Tetris 2077")
 
 # Создание объектов игры и обработчика ввода
 game = Game()
+board = Board()
 input_handler = InputHandler()
 
 # Основной игровой цикл
@@ -23,6 +24,7 @@ while running:
     game.update()  # обновление состояния игры
 
     screen.fill((0, 0, 0))
+    board.draw_grid(screen)
 
     pygame.display.flip()
 
